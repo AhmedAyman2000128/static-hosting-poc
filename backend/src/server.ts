db21840f07b27e047553da5e2c1f906a99637d85
AhmedAyman2000128 <2000128@eng.asu.edu.eg>
2024-10-11 22:52:46 +0300

    back with front connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,34 +1,34 @@
-import { configDotenv } from 'dotenv';
+import { configDotenv } from "dotenv";
 configDotenv();
-import fs from 'fs';
-import express from 'express';
-import upload from './upload';
-import { v4 as uuidv4 } from 'uuid'
-import { createContainer, uploadBlob } from './blobStorage';
-import { unzipFile } from './unzip';
-import path from 'path';
-import mime from 'mime-types';
-import cors from 'cors';
+import fs from "fs";
+import express from "express";
+import upload from "./upload";
+import { v4 as uuidv4 } from "uuid";
+import { createContainer, uploadBlob } from "./blobStorage";
+import { unzipFile } from "./unzip";
+import path from "path";
+import mime from "mime-types";
+import cors from "cors";
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const directoryPath = `temp`;
 
-app.post('/upload', upload.single('image'), async (req, res) => {
+app.post("/upload", upload.single("zipfile"), async (req, res) => {
   const zipFile = req.file as Express.Multer.File;
-  console.log('file: ', zipFile);
+  console.log("file: ", zipFile);
 
-  if(!fs.existsSync(directoryPath)) fs.mkdirSync(directoryPath);
+  if (!fs.existsSync(directoryPath)) fs.mkdirSync(directoryPath);
 
   await unzipFile(zipFile.path, directoryPath);
 
   const containerName = uuidv4().slice(0, 8);
 
   const url = await createContainer(containerName);
-  console.log('container url:', url);
+  console.log("container url:", url);
 
   const fileUrls = [];
 
@@ -36,19 +36,19 @@ app.post('/upload', upload.single('image'), async (req, res) => {
   for await (let file of fs.readdirSync(directoryPath)) {
     // Calculate the full file path
     const filePath = path.join(directoryPath, file);
-  
+
     // Get the MIME type of the file
-    const mimeType = mime.lookup(filePath) || 'application/octet-stream'; // Default if MIME type is not found
-  
-    console.log('File path:', filePath);
-    console.log('MIME type:', mimeType);
+    const mimeType = mime.lookup(filePath) || "application/octet-stream"; // Default if MIME type is not found
+
+    console.log("File path:", filePath);
+    console.log("MIME type:", mimeType);
 
     const fileUrl = await uploadBlob(containerName, file, {
       mimetype: mimeType,
       path: filePath,
     });
 
-    console.log('file url:', fileUrl);
+    console.log("file url:", fileUrl);
     fileUrls.push(fileUrl);
   }
 
