fa3173e76ce0cdeb16f4104283848aa4ae85e696
AhmedAyman2000128 <2000128@eng.asu.edu.eg>
2024-10-11 17:28:22 +0300

    initial commit

---
+++ frontend/src/layout/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+
+function RootLayout() {
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
