a524591ad2862b187489ef0481c9352cc6f5ff4e
AhmedAyman2000128 <2000128@eng.asu.edu.eg>
2024-10-13 19:42:41 +0300

    links modified + nginx.conf

--- frontend/src/Urls/UrlsContainer.jsx
+++ frontend/src/Urls/UrlsContainer.jsx
@@ -13,10 +13,13 @@ function UrlsContainer() {
         </NavLink>
         <div className="flex flex-col gap-4 bg-blue-950 mt-2 centering p-5">
           {urlState.urls.map((url, index) => {
+            const splittedUrl = url.split("/");
+            const newUrl =
+              splittedUrl[splittedUrl.length - 2] + ".astrocloud.com";
             return (
               <p key={index} className="text-center pb-1">
-                <a href={url} target="_blank">
-                  {index + 1}. {url}
+                <a href={"http://" + newUrl} target="_blank">
+                  {index + 1}. {newUrl}
                 </a>
               </p>
             );
