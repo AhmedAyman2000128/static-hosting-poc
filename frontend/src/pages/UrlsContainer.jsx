746cce78bdd13556cabc5e6c4f7df5fd5b9e9681
NightKnight <Ahmedatwa866@yahoo.com>
2024-10-18 11:55:27 +0300

    remove commented code for clarity

--- frontend/src/pages/UrlsContainer.jsx
+++ frontend/src/pages/UrlsContainer.jsx
@@ -3,7 +3,6 @@ import logo from "../images/logo.png";
 import { NavLink } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 function UrlsContainer() {
-  // const urlState = useSelector((state) => state.url);
   const urlData = useLoaderData();
   if (urlData.length === 0) {
     return null;
@@ -14,18 +13,6 @@ function UrlsContainer() {
           <img alt="" src={logo} width={150} />
         </NavLink>
         <div className="flex flex-col gap-4 bg-blue-950 mt-2 centering p-5">
-          {/* {urlState.urls.map((url, index) => {
-            const splittedUrl = url.split("/");
-            const newUrl =
-              splittedUrl[splittedUrl.length - 2] + ".lvh.me";
-            return (
-              <p key={index} className="text-center pb-1">
-                <a href={"http://" + newUrl} target="_blank">
-                  {index + 1}. {newUrl}
-                </a>
-              </p>
-            );
-          })} */}
           {urlData.map((url, index) => {
             console.log(url.name);
             return (
