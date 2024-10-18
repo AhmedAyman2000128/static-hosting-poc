2bf5122d1b4877f5799fa2740637f5f85b0149e8
NightKnight <Ahmedatwa866@yahoo.com>
2024-10-18 11:48:04 +0300

    Remove redundant layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { queryUrls } from "./Urls/UrlsContainer";
-import RootLayout from "./layout/RootLayout";
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
 import UrlsContainer from "./Urls/UrlsContainer";
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
+    <>
       <Route index element={<Home />} loader={queryUrls} />
       <Route path="fileUpload" element={<Upload />} />
       <Route
@@ -19,11 +18,15 @@ const router = createBrowserRouter(
         element={<UrlsContainer />}
         loader={queryUrls}
       />
-    </Route>
+    </>
   )
 );
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <main>
+      <RouterProvider router={router} />
+    </main>
+  );
 }
 
 export default App;
